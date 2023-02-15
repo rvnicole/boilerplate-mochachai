38dbd1f8b3d14314bd62ba7b032b8ea4dae8757d
Nicole <ceinnova05476@innovaccion.mx>
2023-02-14 22:28:02 -0600

    Functional Tests #3

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -36,9 +36,12 @@ suite('Functional Tests', function () {
       chai
         .request(server)
         .put('/travellers')
-
-        .end(function (err, res) {
-          assert.fail();
+        .send( { surname : "Colombo" } )
+        .end( function (err, res) {
+          assert.equal( res.status, 200 );
+          assert.equal( res.type, "application/json" );
+          assert.equal( res.body.name, "Cristoforo" );
+          assert.equal( res.body.surname, "Colombo" );
 
           done();
         });
